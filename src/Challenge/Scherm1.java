701dc00a17bbcca549800a257c85bd2637fc4ae4
ZoeMazeyrac <94126074+ZoeMazeyrac@users.noreply.github.com>
2021-11-15 17:57:53 +0100

    Add files via upload

---
+++ src/Challenge/Scherm1.java
@@ -0,0 +1,13 @@
+package Challenge;
+
+import javax.swing.*;
+
+public class Scherm1 {
+    private JButton homeButton;
+    private JButton aboutUsButton;
+    private JButton shopButton;
+    private JButton supportButton;
+    private JButton searchButton;
+    private JButton cartButton;
+    private JButton accountButton;
+}
